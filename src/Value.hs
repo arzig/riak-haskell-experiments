5d97c5753b4c1bbfb5b92f0a56e7232fee85f8f3
Ryan Zeigler <r.zeigler@icloud.com>
2015-05-27 11:46:37 -0500

    Value example works.
    Now a function

--- src/Value.hs
+++ src/Value.hs
@@ -5,17 +5,14 @@
 module Value where
 
 import           Control.Applicative
-import           Control.Monad
 import           Data.Aeson
 import           Data.ByteString.Char8 (pack)
 import qualified Data.ByteString.Lazy  as L
-import qualified Data.Text             as T
 import           GHC.Generics
 import qualified Network.Riak          as Riak
 import           Network.Riak.Types
-import           System.Environment    (getArgs)
 
-data Dummy = Dummy { a :: Double }
+data Dummy = Dummy { a :: Integer }
     deriving (Show, Generic)
 
 instance ToJSON Dummy
@@ -23,14 +20,13 @@ instance ToJSON Dummy
 instance FromJSON Dummy
 
 instance Riak.Resolvable Dummy where
-    resolve a b = a
+    resolve x _ = x
 
 byteString :: String -> L.ByteString
 byteString s = L.fromChunks ((pack s):[])
 
-main = do
-    let client = Riak.defaultClient
-    conn <- Riak.connect client
-    bucket:key:_ <- getArgs
-    res <- Riak.get conn (byteString bucket) (byteString key) Riak.Default :: IO (Maybe (Dummy, VClock))
-    putStrLn $ show res
+fetch :: String -> String -> IO (Maybe (Dummy, VClock))
+fetch bucket key = do
+    conn <- Riak.connect Riak.defaultClient
+    Riak.get conn (byteString bucket) (byteString key) Riak.Default
+    
