9b50eeec6c4e82423947d63954ebcad65a3e94d6
Ryan Zeigler <r.zeigler@icloud.com>
2015-05-27 07:10:38 -0500

    Use GHC.Generics.

--- src/Value.hs
+++ src/Value.hs
@@ -1,16 +1,26 @@
+{-# LANGUAGE FlexibleContexts, UndecidableInstances, OverloadedStrings, DeriveGeneric #-}
 module Value where
 
-import              Data.String
+import              Control.Monad
+import              Control.Applicative
+import              Data.Aeson
+import qualified    Data.Text as T
 import qualified    Network.Riak as Riak
 import              Network.Riak.Types
 import qualified    Data.ByteString.Lazy as L
 import              Data.ByteString.Char8
                         (pack) 
 import              System.Environment (getArgs)
+import              GHC.Generics
 
-instance Show String
+data Dummy = Dummy { a :: Double }  
+    deriving (Show, Generic)
 
-instance Riak.Resolvable String where
+instance ToJSON Dummy
+
+instance FromJSON Dummy
+
+instance (Show Dummy) => Riak.Resolvable Dummy where
     resolve a b = a
 
 byteString :: String -> L.ByteString
@@ -20,5 +30,5 @@ main = do
     let client = Riak.defaultClient
     conn <- Riak.connect client
     bucket:key:_ <- getArgs
-    res <- Riak.get conn (byteString bucket) (byteString key) Riak.Default :: IO (Maybe (String, VClock))
+    res <- Riak.get conn (byteString bucket) (byteString key) Riak.Default :: IO (Maybe (Dummy, VClock))
     putStrLn $ show res
